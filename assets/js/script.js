1d24e0574f657ea91a8294048d347042448a5e16
Tomidzeri <77193054+Tomidzeri@users.noreply.github.com>
2023-11-03 17:55:37 +0100

    empty commit

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the form element and the submit button
+    var form = document.querySelector('.work-request');
+    var submitButton = form.querySelector('input[type="submit"]');
+  
+    // Add a click event listener to the submit button
+    submitButton.addEventListener('click', function (e) {
+      e.preventDefault(); // Prevent the form from submitting
+  
+      // Show a pop-up message
+      alert('Currently in development...');
+    });
+  });
+  
